8366d03ccec349c604d1896b6bbe35a15d573fcf
bsorrentino <bartolomeo.sorrentino@gmail.com>
2023-09-05 17:54:40 +0200

    feat: add pilot implementation

---
+++ copilot-cli.ts
@@ -0,0 +1,89 @@
+import { ChatOpenAI } from "langchain/chat_models/openai";
+import 'dotenv/config'
+import { DynamicTool } from "langchain/tools";
+
+// [Emulated multi-function calls within one request](https://community.openai.com/t/emulated-multi-function-calls-within-one-request/269582)
+const main = async () => {
+
+    const function_descriptions = [
+        {
+            name: "export_solution",
+            description: "export dataverse solution from remote environment to local file system",
+            parameters: {
+                type: "object",
+                properties: {
+                    solution: {
+                        type: "string",
+                        description: "the remote solution name",
+                    }
+                },
+                required: ["solution"],
+            },
+        },
+        {
+            name: "import_solution",
+            description: "import a dataverse solution to remote environment",
+            parameters: {
+                type: "object",
+                properties: {
+                    path: {
+                        type: "string",
+                        description: "the solution path",
+                    },
+                    environment: {
+                        type: "string",
+                        description: "the target environment",
+                    },
+                    type: {
+                        type: "string",
+                        description: "the solution type",
+                        enum: ["managed", "unmanaged", "both"]
+                    },
+                },
+                required: ["path", "environment"],
+            },
+        }
+    ];
+
+    const CLITool = new DynamicTool({
+        name: "CLI Executor",
+        description:
+            "execute a function as cli command",
+        func: async (input) => {
+            console.debug("input", input)
+            return "tool executed!"
+        },
+    })
+
+    const model = new ChatOpenAI({
+        // modelName: "gpt-4",
+        modelName: "gpt-3.5-turbo-0613",
+    }).bind({
+        functions: function_descriptions,
+
+    });
+
+    let result = await model.invoke(
+        `import the solution 'mysolution.zip' from path '/tmp' in environment 'bartolo' as unmanaged`
+    );
+    console.log(result.additional_kwargs.function_call);
+
+    result = await model.invoke(
+        "export solution 'test' and import it from path '/tmp' in environment 'bartolo' as unmanaged"
+    );
+    console.log(result.additional_kwargs.function_call);
+    
+    result = await model.invoke(
+        "what's the powerapps cli commands to export solution 'test' and import it from path '/tmp' in environment 'bartolo' as unmanaged"
+    );
+    console.log(result.additional_kwargs.function_call);
+
+    result = await model.invoke(
+        "what are the powerapps cli commands"
+    );
+    console.log(result.additional_kwargs.function_call);
+    console.log(result.lc_kwargs.content);
+
+}
+
+main();
---
+++ sample-function-calling.ts
@@ -0,0 +1,46 @@
+import { ChatOpenAI } from "langchain/chat_models/openai";
+import { HumanMessage } from "langchain/schema";
+import 'dotenv/config'
+
+const main = async () => {
+
+    const extractionFunctionSchema = {
+        name: "extractor",
+        description: "Extracts fields from the input.",
+        parameters: {
+          type: "object",
+          properties: {
+            tone: {
+              type: "string",
+              enum: ["positive", "negative"],
+              description: "The overall tone of the input",
+            },
+            word_count: {
+              type: "number",
+              description: "The number of words in the input",
+            },
+            chat_response: {
+              type: "string",
+              description: "A response to the human's input",
+            },
+          },
+          required: ["tone", "word_count", "chat_response"],
+        },
+      };
+      
+    const model = new ChatOpenAI({
+        modelName: "gpt-4",
+      }).bind({
+        functions: [extractionFunctionSchema],
+        function_call: { name: "extractor" },
+      });
+      
+    const result = await model.invoke([
+        new HumanMessage("What a beautiful day!"),
+        new HumanMessage("such bad day!")
+    ]);
+      
+    console.log(result);
+}
+
+main();
