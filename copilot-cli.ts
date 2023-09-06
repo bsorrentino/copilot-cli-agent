ec73731910e5e6ffb4e0c2454c97581feefc8851
bsorrentino <bartolomeo.sorrentino@gmail.com>
2023-09-06 23:38:01 +0200

    feat: finalise agent implementation

--- copilot-cli.ts
+++ copilot-cli.ts
@@ -2,6 +2,7 @@ import { ChatOpenAI } from "langchain/chat_models/openai";
 import 'dotenv/config'
 import { DynamicTool } from "langchain/tools";
 import { Client as Hub } from 'langchainhub'
+import { initializeAgentExecutorWithOptions } from "langchain/agents";
 
 // [Emulated multi-function calls within one request](https://community.openai.com/t/emulated-multi-function-calls-within-one-request/269582)
 const main = async () => {
@@ -59,11 +60,12 @@ const main = async () => {
     const model = new ChatOpenAI({
         // modelName: "gpt-4",
         modelName: "gpt-3.5-turbo-0613",
-    }).bind({
+    })
+    .bind({
         functions: function_descriptions,
 
     });
-
+  
     let result = await model.invoke(
         `import the solution 'mysolution.zip' from path '/tmp' in environment 'bartolo' as unmanaged`
     );
