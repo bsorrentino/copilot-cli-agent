21929f1d75441a9e4e6c0b6c36879fe8988bbe6e
bsorrentino <bartolomeo.sorrentino@gmail.com>
2023-09-07 19:13:28 +0200

    feat: varius improvement
    move from DynamicStructure Agent to specialized class
    introduce prompt template
    add error parsing handler

--- copilot-cli-agent.ts
+++ copilot-cli-agent.ts
@@ -1,80 +1,88 @@
-import { OpenAI } from 'langchain';
+import { PromptTemplate } from 'langchain/prompts';
 import { initializeAgentExecutorWithOptions } from "langchain/agents";
-import { DynamicStructuredTool, DynamicTool } from 'langchain/tools';
-import { FunctionMessage } from 'langchain/schema'
+import { StructuredTool, Tool } from 'langchain/tools';
+import { ChatOpenAI } from 'langchain/chat_models/openai';
+import { CallbackManagerForToolRun } from 'langchain/callbacks';
+// import { FunctionMessage } from 'langchain/schema'
 import 'dotenv/config'
 import z from 'zod'
-import { ChatOpenAI } from 'langchain/chat_models/openai';
 import { intro, outro, text } from '@clack/prompts';
 import 'zx/globals'
 
-/**
- * Langchain tool
- * Exports a Dataverse solution from a remote environment. 
- * 
- * @param {Object} input - Input parameters.
- * @param {string} input.solution - The name of the remote solution to export.
- * 
- * @returns {string} Confirmation message that the export was executed.
- */
-const exportSolutionTool = new DynamicStructuredTool({
-  name: "export_solution",
-  description: "export dataverse solution from remote environment to local file system",
-  schema: z.object({
-    solution: z.string().describe("the remote solution name")
-  }),
-  func: async ( input ) => {
-    console.debug( "Export Solution:", input)
+const ExportSchema = z.object({
+  solution: z.string().describe("the remote solution name")
+});
+class ExportSolutionTool extends StructuredTool<typeof ExportSchema> {
+
+  name = "export_solution"
+  description = "export dataverse solution from remote environment to local file system"
+  schema = ExportSchema
+  
+  async _call(arg: z.output<typeof ExportSchema>, runManager?: CallbackManagerForToolRun): Promise<string> {
+    console.debug( "Export Solution:", arg.solution)
     // return "export executed! please revise prompt removing import command"
     return "export executed!"
-  },
-})
 
-const importSolutionTool = new DynamicStructuredTool({
-  name: "import_solution",
-  description: "import a dataverse solution to remote environment",
-  schema: z.object({
-    path:         z.string()
-                    .describe("the solution path"),
-    environment:  z.string()
-                    .describe("the target environment"),
-    type:         z.enum( ["managed", "unmanaged", "both"] )
-                    .describe( "the solution type" )
-                    .optional()
-                    .default("managed"),
-  }),
-  func: async ( input ) => {
-    console.debug( "Import Solution:", input)
+  }
+}
+
+const ImportSchema = z.object({
+  path:         z.string()
+                  .describe("the solution path"),
+  environment:  z.string()
+                  .describe("the target environment"),
+  type:         z.enum( ["managed", "unmanaged", "both"] )
+                  .describe( "the solution type" )
+                  .optional()
+                  .default("managed"),
+})
+class ImportSolutionTool extends StructuredTool<typeof ImportSchema> {
+
+  name = "import_solution"
+  description = "import a dataverse solution to remote environment"
+  schema = ImportSchema
+
+  async _call(arg: z.output<typeof ImportSchema>, runManager?: CallbackManagerForToolRun): Promise<string> {
+    console.debug( "Import Solution:", arg)
     // return "import executed! please revise prompt removing import command"
     return "import executed!"
-  },
-})
+  }
+}
 
-const systemCommandTool = new DynamicStructuredTool({
-  name: "unknown_cmd",
-  description: "all system commands",
-  schema: z.object({
-    cmd: z.string().describe("the provided command"),
-  }),
-  func: async ( input ) => {
-    console.debug( "System Command:", input)
+class SystemCommandTool extends Tool {
+  name ="system_cmd"
+  description = "all system commands"
+
+  protected async _call(arg: any, runManager?: CallbackManagerForToolRun | undefined): Promise<string> {
+    console.debug( "System Command:", arg)
     // return "import executed! please revise prompt removing import command"
-    return "quit"
-  },
-})
+    return "command executed!"
+  }
+
+}
 
 const main = async () => {
      
   const model = new ChatOpenAI({ 
       // modelName: "gpt-4",
       modelName: "gpt-3.5-turbo-0613",
-      stop: ["end", "stop", "quit"],
+      // stop: ["end", "stop", "quit"],
       temperature: 0});
   
-  const tools = [ exportSolutionTool, importSolutionTool, systemCommandTool ];
+  const tools = [ 
+    new SystemCommandTool(),
+    new ExportSolutionTool(), 
+    new ImportSolutionTool(), 
+     ];
   
   const agent = await initializeAgentExecutorWithOptions(tools, model, {
       agentType: "openai-functions",
+      verbose: false,
+      handleParsingErrors: ( e ) => {
+
+        console.error("HANDLE ERROR", e);
+        return "there is an error!"
+      }
   });
 
   intro(`let begin our conversation`);
@@ -88,16 +96,19 @@ const main = async () => {
     },
   });
 
-  //console.debug( input )
-
+  const promptTemplate = PromptTemplate.fromTemplate(
+    "assuming that we are on {platform} operative system: \n\n{input}"
+  );
+  
   $.verbose = false
   try {
-    const result = await agent.call({ input });
-    console.log(result.output);
+    const prompt = await promptTemplate.format( { platform: os.platform(), input: input })
+    const result = await agent.run( prompt );
+    console.log(result);
   
   }
   catch( e:any ) {
-    console.error( e.message )
+    console.error( e )
   }
 
 }
