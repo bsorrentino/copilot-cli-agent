b4e45462f7cafd9765b9a032575dedeeb1a659b3
bsorrentino <bartolomeo.sorrentino@gmail.com>
2023-11-21 20:10:51 +0100

    feat: add a new custom command

---
+++ commands/PlantUMLSpriteGenerator.mjs
@@ -0,0 +1,25 @@
+import { z } from "zod";
+import { CommandTool, runCommand } from "copilot-cli-core";
+var GrayLevel;
+(function (GrayLevel) {
+    GrayLevel["Four"] = "4";
+    GrayLevel["Eight"] = "8";
+    GrayLevel["Sixteen"] = "16";
+})(GrayLevel || (GrayLevel = {}));
+const schema = z.object({
+    imagePath: z.string(),
+    grayLevel: z.enum([GrayLevel.Four, GrayLevel.Eight, GrayLevel.Sixteen]).optional().default(GrayLevel.Sixteen),
+});
+class PlantUMLSpriteGeneratorTool extends CommandTool {
+    name = "plantUMLSpriteGenerator";
+    description = "Generate a plantuml sprite from image.";
+    schema = schema;
+    async _call(arg) {
+        await runCommand({
+            cmd: `plantuml -encodesprite ${arg.grayLevel} ${arg.imagePath}`,
+            out: `${arg.imagePath}.puml`
+        }, this.execContext);
+        return `plantuml sprite ${arg.imagePath}.puml generated`;
+    }
+}
+export default new PlantUMLSpriteGeneratorTool();
