e48a981250bd8852c437951868a373d7201fa6a6
bsorrentino <bartolomeo.sorrentino@gmail.com>
2024-01-15 16:47:07 +0100

    build: update dist files

--- packages/simple/lib/simple-cli.js
+++ packages/simple/lib/simple-cli.js
@@ -9,11 +9,12 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const main = async () => {
     // const _modules = await scanFolderAndImportPackage( path.join( __dirname, 'commands') );
-    const commandPath = process.env['COMMANDS_PATH'];
-    if (!commandPath) {
-        throw new Error("'COMMANDS_PATH' environment variable is not defined!");
+    let commandsPath = process.env['COMMANDS_PATH'];
+    if (!commandsPath) {
+        commandsPath = path.join(process.cwd(), 'commands');
+        p.log.warning(`'COMMANDS_PATH' environment variable is not defined! the commands path is set to ${commandsPath} by default`);
     }
-    const _modules = await scanFolderAndImportPackage(commandPath);
+    const _modules = await scanFolderAndImportPackage(commandsPath);
     const progress = p.spinner();
     const execContext = {
         history: new CommandHistory(),
